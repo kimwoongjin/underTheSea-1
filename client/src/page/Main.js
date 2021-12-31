891777c4fc9c6213565cbdc2093ebcce819bcb22
jeongseongwoo <aqualist2011@naver.com>
2021-12-31 16:57:24 +0900

    Add: mainpage(header,landing1)

---
+++ client/src/page/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../component/Header";
+import Landingpage from "../Landing_component/Landingpage";
+
+function Main() {
+  return (
+    <>
+      <Header></Header>
+      <Landingpage></Landingpage>
+    </>
+  );
+}
+
+export default Main;
