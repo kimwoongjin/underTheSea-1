a8aaa0cc3cdeaa2fee98803ced74a0a1a90f30fe
pgj0116 <83294916+pgj0116@users.noreply.github.com>
2022-01-07 18:26:16 +0900

    Fix: Status Code

--- server/controllers/user/usertips.js
+++ server/controllers/user/usertips.js
@@ -5,7 +5,7 @@ module.exports = async (req, res) => {
   const userinfo = isAuthorized(req);
 
   if (!userinfo) {
-    return res.status(401).json({ message: "You are not authorized" });
+    return res.status(200).json({ message: "You are not authorized" });
   } else {
     const user_id = userinfo.id;
 
