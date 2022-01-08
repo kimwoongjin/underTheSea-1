c738a9781db38c571e0afe1de21bbe94ff306ce0
yesjin <woong1536@gmail.com>
2022-01-08 17:18:33 +0900

    Edit : status code start.sh

--- server/controllers/user/login.js
+++ server/controllers/user/login.js
@@ -5,11 +5,11 @@ module.exports = async (req, res) => {
   const { email, user_pwd } = req.body.data;
 
   if (!email || !user_pwd) {
-    return res.status(200).json({ message: "Not enough data" });
+    return res.status(400).json({ message: "Not enough data" });
   } else {
     const user = await users.findOne({ where: { email, user_pwd } });
     if (!user) {
-      return res.status(200).json({ message: "You don't have an account yet" });
+      return res.status(401).json({ message: "You don't have an account yet" });
     } else {
       delete user.dataValues.user_pwd;
 
