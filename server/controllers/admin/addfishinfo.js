7af1a50a770c9a206b4f069a92c3fd2377c5d3a8
pgj0116 <83294916+pgj0116@users.noreply.github.com>
2022-01-12 09:13:00 +0900

    Add: SSR file

--- server/controllers/admin/addfishinfo.js
+++ server/controllers/admin/addfishinfo.js
@@ -1,15 +1,22 @@
-const { fishes, not_togethers } = require("../../models");
+const { fishes } = require("../../models");
 const { isAuthorized } = require("../tokenFunction");
 
 module.exports = async (req, res) => {
   // const userInfo = isAuthorized(req);
-  // //console.log(userInfo);
-  // //console.log(req.body);
   // if (!userInfo) {
   //   return res.status(401).json({ message: "You are not authorized" });
   // }
-  console.log(req.body);
-  const { fish_name, habitant, temp } = req.body;
+  const {
+    fish_name,
+    habitant,
+    temp,
+    desc,
+    fish_img,
+    fresh_water,
+    reefsafe,
+    size,
+    sci_name,
+  } = req.body;
   const check_new_fish = await fishes.findOne({ where: { fish_name } });
   if (check_new_fish) {
     return res.status(400).json({ message: "The fish is already in DB" });
@@ -18,15 +25,19 @@ module.exports = async (req, res) => {
       fish_name,
       habitant,
       temp,
+      desc,
+      fish_img,
+      fresh_water,
+      reefsafe,
+      size,
+      sci_name,
     });
 
     const { id } = new_fish.dataValues;
-    console.log(new_fish.dataValues);
 
     return res.status(201).json({
-      data: new_fish,
+      data: new_fish.dataValues,
       message: "Fish data is successfully added",
     });
-    //notTogether 배열을 다 돌면서 notTogether 리스트에 추가하기
   }
 };
