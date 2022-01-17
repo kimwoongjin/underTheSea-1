f818464d5bd7c178e2f686611c147ab83ac57f4e
pgj0116 <83294916+pgj0116@users.noreply.github.com>
2022-01-17 16:49:24 +0900

    Removed node_modules folder

--- server/routes/admin.js
+++
@@ -1,9 +0,0 @@
-const controllers = require("../controllers");
-const express = require("express");
-const router = express.Router();
-
-router.post("/fish", controllers.addfishinfo);
-router.patch("/fish/:fish_name", controllers.editfishinfo);
-router.delete("/fish/:fish_name", controllers.deletefishinfo);
-
-module.exports = router;
--- server/routes/comment.js
+++
@@ -1,9 +0,0 @@
-const controllers = require("../controllers");
-const express = require("express");
-const router = express.Router();
-
-router.post("/:tip_id", controllers.addcomment);
-router.delete("/:comment_id", controllers.deletecommnet);
-router.get("/:tip_id", controllers.tipscomment);
-
-module.exports = router;
