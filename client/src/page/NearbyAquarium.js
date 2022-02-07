44534f38b66a953fc41bdcf8f6851560cc0d4fc1
jeongseongwoo <aqualist2011@naver.com>
2022-02-07 14:54:50 +0900

    Add: inputBox

--- client/src/page/NearbyAquarium.js
+++ client/src/page/NearbyAquarium.js
@@ -16,6 +16,22 @@ const MapContainer = styled.div`
   width: 100vw;
   height: 100vh;
   border: 1px solid red;
+  position: relative;
+`;
+
+const Input = styled.input``;
+
+const Button = styled.button``;
+
+const SearchContainer = styled.div`
+  position: absolute;
+  top: 1%;
+  left: 1%;
+  width: 15%;
+  height: 40px;
+  border: 1px solid red;
+  background: red;
+  z-index: 99;
 `;
 
 function NearbyAquarium() {
@@ -25,47 +41,19 @@ function NearbyAquarium() {
     level: 3, //지도의 레벨(확대, 축소 정도)
   };
   const container = useRef(null); //지도를 담을 영역의 DOM 레퍼런스
-  // 마커배열
-  //   let markers = [];
-
-  // 지도생성
-  //   let map = new window.kakao.maps.Map(container.current, options);
-
-  // 장소 검색 객체
-  //   let ps = new window.kakao.maps.services.places();
-
-  // 검색결과 목록이나 마커를 클릭했을 때 장소명을 표출할 윈도우 인포를 생성
-  //   let infowindow = new window.kakao.maps.InfoWindow({ zIndex: 1 });
-
-  //   const searchPlaces = () => {
-  //       let keyword =
-  //   }
 
   useEffect(() => {
     new window.kakao.maps.Map(container.current, options); //지도 생성 및 객체 리턴
-    // var container = document.getElementById('map');
-    // var options = {
-    //   center: new kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
-    //   level: 3
-    // };
-
-    var map = new kakao.maps.Map(container, options);
-    var markerPosition = new kakao.maps.LatLng(
-      37.365264512305174,
-      127.10676860117488
-    );
-    var marker = new kakao.maps.Marker({
-      position: markerPosition,
-    });
-    marker.setMap(map);
-
     return () => {};
   }, []);
 
   return (
     <Container>
       <Header2 />
-      <MapContainer className="map" ref={container}></MapContainer>
+
+      <MapContainer className="map" ref={container}>
+        <SearchContainer></SearchContainer>
+      </MapContainer>
       <Footer />
     </Container>
   );
